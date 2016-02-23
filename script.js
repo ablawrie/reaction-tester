531ed94eb7893a52abb0a5dc89fa574655ecc34d
Arthur Lawrie <ablawrie@gmail.com>
2016-02-23 12:36:35 -0500

    Update script.js
    Change shape and cleaned up code. Simplified the position functions because the shapes kept spawning outside of the window.

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-var createdTime,
-	clickedTime,
-	reactionTime;
-
-function makeBox(){
-	var s = document.getElementById("box");
-	//Timestamp
-	createdTime = Date.now();
-	var x = Math.random();
-	x = x * 5000;
-	//Color Value
+//Color Value
+function getRandomColor(){
 	var rgb = [0, 0, 0];
 	for(var  i = 0; i < 3; i++){
 		rgb[i] = Math.floor((Math.random() * 255) + 1);
 	}
 	var color = "rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")";
-	//Get Screen Size
-	var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
-	//Change Position
-	s.style.left = Math.floor((Math.random() * x) + 1)+"px";
-	s.style.top = Math.floor((Math.random() * y) + 1)+"px";
-	//Shape Creation
-	s.style.backgroundColor = color;
-	setTimeout(function(){ s.style.display = "block";}, x);
+	return color;
 }
-
-document.getElementById("box").onclick=function(){
+//Shape
+function getCircle(){
+	if (Math.random() > 0.5){
+		return "100px";
+	} else {
+		return "0";
+	}
+}
+var createdTime,
+	clickedTime,
+	reactionTime;
+function makeShape(){
+	var s = document.getElementById("shape");
+	//Timestamp
+	createdTime = Date.now();
+	var x = Math.random();
+	x = x * 5000;
+	//Change Position
+	var py = Math.random();
+	py = py * 720;
+	var px = Math.random();
+	px = px * 1280;
+	//Shape Creation
+	setTimeout(function(){ 
+		s.style.display = "block";
+		s.style.borderRadius = getCircle();
+		s.style.backgroundColor = getRandomColor();
+		s.style.top = py+"px";
+		s.style.left = px+"px";
+	}, x);
+}
+document.getElementById("shape").onclick=function(){
 	clickedTime = Date.now();
 	reactionTime = (clickedTime - createdTime) / 1000;
 	document.getElementById("time").innerHTML = reactionTime;
 	this.style.display="none";
-	makeBox();
+	makeShape();
 }
-
-makeBox();
+makeShape();
